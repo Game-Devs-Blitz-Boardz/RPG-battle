572acf12e1aca4384c578d857892feffa912acad
lillybo <lillian.bojkov@gmail.com>
2023-08-18 17:13:22 +0300

    Adding collision check to ground for control of jump state

--- StateMachine/PlayerGroundedState.cs
+++ StateMachine/PlayerGroundedState.cs
@@ -20,7 +20,7 @@ public class PlayerGroundedState : PlayerState
     public override void Update() {
         base.Update();
 
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space) && player.IsGroundDetected()) {
             stateMachine.ChangeState(player.jumpState);
         }
     }

df8bd0175e393eb92df16358fece1b286dfb3c03
lillybo <lillian.bojkov@gmail.com>
2023-08-19 23:09:22 +0300

    Creating enemy finite state machine - basics

---
+++ Enemy/StateMachine/EnemyStateMachine.cs
@@ -0,0 +1,19 @@
+public class EnemyStateMachine
+{
+
+    public EnemyState currentState { get; private set; }
+
+    public void Initialize(EnemyState _startingState)
+    {
+        currentState = _startingState;
+        currentState.Enter();
+    }
+
+    public void ChangeState(EnemyState _newState)
+    {
+        currentState.Exit();
+        currentState = _newState;
+        currentState.Enter();
+    }
+
+}

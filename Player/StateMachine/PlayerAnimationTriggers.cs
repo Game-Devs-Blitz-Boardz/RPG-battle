6ece77555f92cdaecc0bfb02333b380801b3eb63
lillybo <lillian.bojkov@gmail.com>
2023-08-30 01:59:23 +0300

    Setting up sword details

--- Player/StateMachine/PlayerAnimationTriggers.cs
+++ Player/StateMachine/PlayerAnimationTriggers.cs
@@ -18,4 +18,8 @@ public class PlayerAnimationTriggers : MonoBehaviour
             }
         }
     }
+
+    private void ThrowSword() {
+        SkillManager.instance.sword.CreateSword();
+    }
 }

605bf7769cbd073ac5cca216c38620a38d630150
lillybo <lillian.bojkov@gmail.com>
2023-09-01 01:07:58 +0300

    Making crystal explosive - does damage + explode anim

--- Skills/Skill_Controllers/Crystal_Skill_Controller.cs
+++ Skills/Skill_Controllers/Crystal_Skill_Controller.cs
@@ -3,12 +3,24 @@ using UnityEngine;
 public class Crystal_Skill_Controller : MonoBehaviour
 {
 
+    private Animator anim => GetComponent<Animator>();
+    private CircleCollider2D cd => GetComponent<CircleCollider2D>();
+
     private float crystalExistTimer;
+
+    private bool canExplode;
+    private bool canMove;
+    private float moveSpeed;
     
-    public void SetupCrystal(float _crystalDuration) {
+    private bool canGrow;
+    private float growSpeed = 5f;
+
+    public void SetupCrystal(float _crystalDuration, bool _canExplode, bool _canMove, float _moveSpeed) {
 
         crystalExistTimer = _crystalDuration;
-
+        canExplode = _canExplode;
+        canMove = _canMove;
+        moveSpeed = _moveSpeed;
 
     }
 
@@ -16,6 +28,29 @@ public class Crystal_Skill_Controller : MonoBehaviour
         crystalExistTimer -= Time.deltaTime;
 
         if (crystalExistTimer < 0) {
+            FinishCrystal();
+        }
+
+        if (canGrow) {
+            transform.localScale = Vector3.Lerp(transform.localScale, new Vector2(3, 3), growSpeed * Time.deltaTime);
+        }
+    }
+
+    private void AnimationExplodeEvent() {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, cd.radius);
+
+        foreach(var hit in colliders) {
+            if (hit.GetComponent<Enemy>() != null) {
+                hit.GetComponent<Enemy>().Damage();
+            }
+        }
+    }
+
+    public void FinishCrystal() {
+        if (canExplode) {
+            canGrow = true;
+            anim.SetTrigger("Explode");
+        } else {
             SelfDestroy();
         }
     }

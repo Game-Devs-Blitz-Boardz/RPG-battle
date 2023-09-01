e56a31e2f5a4128af8f57da5b6f1a1e17ce4161b
lillybo <lillian.bojkov@gmail.com>
2023-09-01 15:37:39 +0300

    Creating character stats

--- Player/StateMachine/PlayerAnimationTriggers.cs
+++ Player/StateMachine/PlayerAnimationTriggers.cs
@@ -14,7 +14,7 @@ public class PlayerAnimationTriggers : MonoBehaviour
 
         foreach(var hit in colliders) {
             if (hit.GetComponent<Enemy>() != null) {
-                hit.GetComponent<Enemy>().Damage();
+                hit.GetComponent<CharacterStats>().TakeDamage(player.stats.damage);
             }
         }
     }

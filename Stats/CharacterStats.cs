f6b65a051766bead39c4dc39f26c9858c16f7b04
lillybo <lillian.bojkov@gmail.com>
2023-09-05 19:49:13 +0300

    Setting up character stats UI

--- Stats/CharacterStats.cs
+++ Stats/CharacterStats.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
 using System.Collections;
 
+public enum StatType {
+    strength,
+    agility,
+    intelligence,
+    vitality,
+    damage,
+    critChance,
+    critPower,
+    health,
+    armour,
+    evasion,
+    magicRes,
+    fireDamage,
+    iceDamage,
+    lightningDamage,
+}
+
+
 public class CharacterStats : MonoBehaviour
 {
 
@@ -360,5 +378,26 @@ public class CharacterStats : MonoBehaviour
         return maxHealth.GetValue() + vitality.GetValue() * 5;
     }
 #endregion
+
+    public Stat GetStat(StatType _statType) {
+
+        if (_statType == StatType.strength) return strength;
+        else if (_statType == StatType.agility) return agility;
+        else if (_statType == StatType.intelligence) return intelligence;
+        else if (_statType == StatType.vitality) return vitality;
+        else if (_statType == StatType.damage) return damage;
+        else if (_statType == StatType.critChance) return critChance;
+        else if (_statType == StatType.critPower) return critPower;
+        else if (_statType == StatType.health) return maxHealth;
+        else if (_statType == StatType.armour) return armour;
+        else if (_statType == StatType.evasion) return evasion;
+        else if (_statType == StatType.magicRes) return magicResistance;
+        else if (_statType == StatType.fireDamage) return fireDamage;
+        else if (_statType == StatType.iceDamage) return iceDamage;
+        else if (_statType == StatType.lightningDamage) return lightningDamage;
+
+        return null;
+
+    }
 
 }
--- UI/UI_EquipmentSlot.cs
+++ UI/UI_EquipmentSlot.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public class UI_EquipmentSlot : UI_ItemSlot
 {
@@ -14,6 +10,8 @@ public class UI_EquipmentSlot : UI_ItemSlot
     }
 
     public override void OnPointerDown(PointerEventData eventData) {
+
+        if (item == null || item.data == null) return;
 
         Inventory.instance.UnequipItem(item.data as ItemData_Equipment);
         Inventory.instance.AddItem(item.data as ItemData_Equipment);

2b4ab18d3be53d9b9ee99cdc0f90cccc9e2cbc5f
lillybo <lillian.bojkov@gmail.com>
2023-09-22 14:40:38 +0300

    Adding audio distance limit

--- Managers/AudioManager.cs
+++ Managers/AudioManager.cs
@@ -7,6 +7,7 @@ public class AudioManager : MonoBehaviour
 
     public static AudioManager instance;
 
+    [SerializeField] private float sfxMinimumDistance;
     [SerializeField] private AudioSource[] sfx;
     [SerializeField] private AudioSource[] bgm;
 
@@ -34,7 +35,12 @@ public class AudioManager : MonoBehaviour
         }
     }
 
-    public void PlaySFX(int _sfxIndex) {
+    public void PlaySFX(int _sfxIndex, Transform _source) {
+
+        if (sfx[_sfxIndex].isPlaying) return;
+
+        if (_source != null && Vector2.Distance(PlayerManager.instance.player.transform.position, _source.position) > sfxMinimumDistance) return;
+        
 
         if (_sfxIndex < sfx.Length) {
             sfx[_sfxIndex].pitch = Random.Range(0.85f, 1.1f);
